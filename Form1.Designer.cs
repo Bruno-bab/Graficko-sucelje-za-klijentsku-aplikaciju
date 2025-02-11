f587c6feefceaf79d7bf93a32e6b6541894762a7
Bruno B. <brbaburi@vsite.hr>
2025-02-11 14:14:38 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Grafičko_sučelje_za_klijentsku_aplikaciju
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            communicationToolStripMenuItem = new ToolStripMenuItem();
+            sendToolStripMenuItem = new ToolStripMenuItem();
+            sendToolStripMenuItem1 = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { communicationToolStripMenuItem, sendToolStripMenuItem, sendToolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1079, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // communicationToolStripMenuItem
+            // 
+            communicationToolStripMenuItem.Name = "communicationToolStripMenuItem";
+            communicationToolStripMenuItem.Size = new Size(128, 24);
+            communicationToolStripMenuItem.Text = "Communication";
+            communicationToolStripMenuItem.Click += communicationToolStripMenuItem_Click;
+            // 
+            // sendToolStripMenuItem
+            // 
+            sendToolStripMenuItem.Name = "sendToolStripMenuItem";
+            sendToolStripMenuItem.Size = new Size(80, 24);
+            sendToolStripMenuItem.Text = "File view";
+            sendToolStripMenuItem.Click += sendToolStripMenuItem_Click;
+            // 
+            // sendToolStripMenuItem1
+            // 
+            sendToolStripMenuItem1.Name = "sendToolStripMenuItem1";
+            sendToolStripMenuItem1.Size = new Size(56, 24);
+            sendToolStripMenuItem1.Text = "Send";
+            sendToolStripMenuItem1.Click += sendToolStripMenuItem1_Click_1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(0, 31);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1079, 472);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1079, 500);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Client Application";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem communicationToolStripMenuItem;
+        private ToolStripMenuItem sendToolStripMenuItem;
+        private ToolStripMenuItem sendToolStripMenuItem1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace Grafičko_sučelje_za_klijentsku_aplikaciju
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void communicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
